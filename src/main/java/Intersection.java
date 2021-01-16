7d81b8c623ecf74b03abd268622b94ded0d0365a
kowalczy <77060169+kowalczy@users.noreply.github.com>
2021-01-16 15:42:50 +0100

    Added method for tests to Intersection.java.

--- src/main/java/Intersection.java
+++ src/main/java/Intersection.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.lang.Math;
 
 public class Intersection {
+
     static class Point {
         int x;
         int y;
@@ -49,13 +50,9 @@ public class Intersection {
     static boolean doIntersect(Point p1, Point q1, Point p2, Point q2) {
 
         int o1 = orientation(p1, q1, p2);
-
         int o2 = orientation(p1, q1, q2);
-
         int o3 = orientation(p2, q2, p1);
-
         int o4 = orientation(p2, q2, q1);
-
 
         if (o1 != o2 && o3 != o4) {
             return true;
@@ -190,6 +187,10 @@ public class Intersection {
             }
         }
         return null;
+    }
+
+    public Point createPoint(int x, int y){
+        return new Point(x,y);
     }
 
 }
