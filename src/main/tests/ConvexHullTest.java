4fb605ee504eef136e84be72206043104c4b8900
kowalczy <77060169+kowalczy@users.noreply.github.com>
2021-01-16 15:41:51 +0100

    Added tests for ConvexHull and Intersection.

--- src/main/tests/ConvexHullTest.java
+++ src/main/tests/ConvexHullTest.java
@@ -1,10 +1,98 @@
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class ConvexHullTest {
+    List<Hospital> hospitalList;
+    List<Monument> monumentList;
+    Double[] inputPoints1;
+    Double[] inputPoints2;
+    Double[] expectedOutputPoints1;
+    Double[] expectedOutputPoints2;
+    ArrayList<Double> borders;
+    @BeforeTest
+    public void initialize() throws FileNotFoundException {
+        InputFileReader inputFileReader = new InputFileReader("src/data/sampleData.txt", false);
+        hospitalList = Objects.requireNonNull(inputFileReader).getHospitalList();
+        monumentList = Objects.requireNonNull(inputFileReader).getMonumentList();
+        inputPoints1 = new Double[]{7.0, 7.0, 7.0, -7.0, -7.0, -7.0, -7.0, 7.0, 9.0, 0.0, -9.0, 0.0, 0.0, 9.0, 0.0, -9.0};
+        expectedOutputPoints1 = new Double[]{-9.0, 0.0, -7.0, -7.0, 0.0, -9.0, 7.0, -7.0, 9.0, 0.0, 7.0, 7.0, 0.0, 9.0, -7.0, 7.0};
+        inputPoints2 = new Double[]{7.0, 7.0, 7.0, -7.0, -7.0, -7.0, -7.0, 7.0, 9.0, 0.0, -9.0, 0.0, 0.0, 9.0, 0.0, -9.0, 0.0, 0.0, 1.0, 2.0, -2.0, 1.0, -1.0, -1.0, 3.0, 4.0, 4.0, 3.0, -5.0, 4.0, 6.0, 5.0};
+        expectedOutputPoints2 = new Double[]{-9.0, 0.0, -7.0, -7.0, 0.0, -9.0, 7.0, -7.0, 9.0, 0.0, 7.0, 7.0, 0.0, 9.0, -7.0, 7.0};
+        borders = ConvexHull.convex_hull(hospitalList,monumentList);
+    }
 
     @Test
-    public void testIsInBorder() {
+    public void testConvexHull1() {
+        List<Double> expectedResult;
+        List<Double> result = new ArrayList<>();
+        List<Hospital> hospitalList = new ArrayList<>();
+        List<Monument> monumentList = new ArrayList<>();
+
+
+        for(int i = 0; i < inputPoints1.length / 2; i += 2){
+            hospitalList.add(new Hospital(i + 1, "Hospital", inputPoints1[i], inputPoints1[i+1], 0, 0));
+        }
+
+        for(int i = inputPoints1.length / 2; i < inputPoints1.length; i += 2){
+            monumentList.add(new Monument(i + 1, "Monument", inputPoints1[i], inputPoints1[i+1]));
+        }
+
+        expectedResult= new ArrayList<Double>(Arrays.asList(expectedOutputPoints1));
+        result = ConvexHull.convex_hull(hospitalList, monumentList);
+
+        Assert.assertEquals(result, expectedResult);
+    }
+
+    @Test
+    public void testConvexHull2() {
+        List<Double> expectedResult;
+        List<Double> result = new ArrayList<>();
+        List<Hospital> hospitalList = new ArrayList<>();
+        List<Monument> monumentList = new ArrayList<>();
+
+
+        for(int i = 0; i < inputPoints2.length / 2; i += 2){
+            hospitalList.add(new Hospital(i + 1, "Hospital", inputPoints2[i], inputPoints2[i+1], 0, 0));
+        }
+
+        for(int i = inputPoints2.length / 2; i < inputPoints2.length; i += 2){
+            monumentList.add(new Monument(i + 1, "Monument", inputPoints2[i], inputPoints2[i+1]));
+        }
+
+        expectedResult= new ArrayList<Double>(Arrays.asList(expectedOutputPoints2));
+        result = ConvexHull.convex_hull(hospitalList, monumentList);
+
+        Assert.assertEquals(result, expectedResult);
+    }
+
+    @Test
+    public void testIsInBorder_shouldReturnTrue(){
+        List<Point> patientList = new ArrayList<>();
+        for (int i = 1; i <= 10; i++){
+            patientList.add(new Point(i*11.0, i + 15.0));
+        }
+
+        for (int i = 0; i < 10; i++){
+            Assert.assertEquals(ConvexHull.isInBorder(borders, patientList.get(i)), true);
+        }
+    }
+
+    @Test
+    public void testIsInBorder_shouldReturnFalse(){
+        List<Point> patientList = new ArrayList<>();
+        for (int i = 1; i <= 10; i++){
+            patientList.add(new Point(i, i - 15.0));
+        }
+
+        for (int i = 0; i < 10; i++){
+            Assert.assertEquals(ConvexHull.isInBorder(borders, patientList.get(i)), false);
+        }
     }
 }
--- src/main/tests/IntersectionTest.java
+++ src/main/tests/IntersectionTest.java
@@ -1,30 +1,91 @@
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import static org.testng.Assert.*;
 
 public class IntersectionTest {
+    List<Hospital> hospitalList;
+    List<Road> roadList;
+    List<Hospital> intersectionList;
+    Intersection intersection;
 
-    @Test
-    public void testOnSegment() {
-    }
+    @BeforeTest
+    public void initialize() throws FileNotFoundException {
+        intersection = new Intersection();
+        hospitalList = new ArrayList<>();
+        roadList = new ArrayList<>();
+        intersectionList = new ArrayList<>();
 
-    @Test
-    public void testOrientation() {
-    }
+        Hospital hospital1 = new Hospital(1, "Szpital Wojewódzki nr 997", 10, 10, 1000, 100);
+        Hospital hospital2 = new Hospital(2, "Krakowski Szpital Kliniczny", 100, 120, 999, 99);
+        Hospital hospital3 = new Hospital(3, "Pierwszy Szpital im. Prezesa RP", 120, 130, 99, 0);
+        Hospital hospital4 = new Hospital(4, "Drugi Szpital im. Naczelnika RP", 10, 140, 70, 1);
+        Hospital hospital5 = new Hospital(5, "Trzeci Szpital im. Króla RP", 140, 10, 996, 0);
+        hospitalList.add(hospital1);
+        hospitalList.add(hospital2);
+        hospitalList.add(hospital3);
+        hospitalList.add(hospital4);
+        hospitalList.add(hospital5);
 
-    @Test
-    public void testDoIntersect() {
-    }
+        Road road1 = new Road(1, hospital1, hospital2, 700);
+        Road road2 = new Road(2, hospital1, hospital4, 550);
+        Road road3 = new Road(3, hospital1, hospital5, 800);
+        Road road4 = new Road(4, hospital2, hospital3, 300);
+        Road road5 = new Road(5, hospital2, hospital4, 550);
+        Road road6 = new Road(6, hospital3, hospital5, 600);
+        Road road7 = new Road(7, hospital4, hospital5, 750);
+        roadList.add(road1);
+        roadList.add(road2);
+        roadList.add(road3);
+        roadList.add(road4);
+        roadList.add(road5);
+        roadList.add(road6);
+        roadList.add(road7);
 
-    @Test
-    public void testFindCrossPoint() {
-    }
-
-    @Test
-    public void testFindIntersections() {
     }
 
     @Test
     public void testGetRoadByHospitals() {
+        Road expected = roadList.get(0);
+
+        Road result = intersection.getRoadByHospitals(roadList, hospitalList.get(0), hospitalList.get(1));
+
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void testDoIntersect_shouldReturnTrue() {
+        assertEquals(Intersection.doIntersect(intersection.createPoint(10,10), intersection.createPoint(100,100), intersection.createPoint(100,10), intersection.createPoint(10,100)),true);
+    }
+
+    @Test
+    public void testDoIntersect_shouldReturnFalse() {
+        assertEquals(Intersection.doIntersect(intersection.createPoint(10,10), intersection.createPoint(100,100), intersection.createPoint(11,10), intersection.createPoint(101,100)),false);
+    }
+
+    @Test
+    public void testFindCrossPoint() {
+        double[] expected = {55.0, 55.0};
+
+        double[] result = Intersection.findCrossPoint(intersection.createPoint(10,10), intersection.createPoint(100,100), intersection.createPoint(100,10), intersection.createPoint(10,100));
+
+        assertTrue(expected[0] == result[0] && expected[1] == result[1]);
+    }
+
+    @Test
+    public void testFindIntersections() {
+        Hospital expected = new Hospital(6,"Intersection 1" ,68.5, 81.5, 0, 0);
+        Hospital result;
+
+        intersection.findIntersections(hospitalList, intersectionList, roadList);
+        result = intersectionList.get(intersectionList.size()-1);
+
+        Assert.assertTrue(expected.getId() == result.getId() && expected.getX() == result.getX() && expected.getY() == result.getY());
     }
 }
